7f836c2a3f827092e38180ab18ad831d5a9d5f31
Benni H <Benni H>
2016-04-21 18:47:53 +0200

    Initial commit

---
+++ app/src/main/java/de/emdb/teuthein/hot_shot_tracker/app/ActivityBurger.java
@@ -0,0 +1,13 @@
+package de.emdb.teuthein.hot_shot_tracker.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityBurger extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_burger);
+    }
+}
